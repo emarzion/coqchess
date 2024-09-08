5cb972baa428ac9c79f64d439703d3dfb5c43e04
emarzion <emarzion@gmail.com>
2024-09-08 15:42:00 -0500

    some work

--- theory/Chess/Examples/QKvK.v
+++ theory/Chess/Examples/QKvK.v
@@ -1,6 +1,6 @@
 Require Import Lia.
 
-Require Import Chess.Chess.
+Require Import Chess.Chess.Chess.
 Require Import Games.Game.Player.
 Require Import Games.Game.Win.
 Require Import Games.Game.Forces.
@@ -8,9 +8,10 @@ Require Import Games.Util.Dec.
 
 Require Import Chess.Util.Fin.
 
-(** QKvK position with white to play *)
+(** QKvK position with white to play or black to play with white queen not
+    being threatened by the black king; and the position is not stalemate.
+*)
 Record t (s : ChessState) : Type := {
-  white_to_play : chess_to_play s = White;
   white_queen : Pos;
   lookup_white_queen : lookup_piece white_queen (board s) =
     Some (White, Queen);
@@ -19,41 +20,98 @@ Record t (s : ChessState) : Type := {
     pos = white_queen \/ pos = white_king s;
   black_pieces : forall pos piece,
     lookup_piece pos (board s) = Some (Black, piece) ->
-    pos = black_king s
+    pos = black_king s;
+  white_queen_unthreatened :
+    chess_to_play s = Black ->
+    neighbor_adj (board s) (black_king s) white_queen ->
+    neighbor_adj (board s) (white_king s) white_queen;
+  not_stalemate :
+    atomic_chess_res s <> Some Game.Draw;
   }.
 
-Arguments white_to_play {_}.
 Arguments white_queen {_}.
 Arguments lookup_white_queen {_}.
 Arguments white_pieces {_}.
 Arguments black_pieces {_}.
-
-Definition rank_val (r : Rank) : nat :=
-  match r with
-  | MkRank i => val i
-  end.
-
-Definition rank_ascend (p1 p2 : Pos) : Prop :=
-  rank_val (rank p1) < rank_val (rank p2).
+Arguments white_queen_unthreatened {_}.
+Arguments not_stalemate {_}.
 
 Module cutoff.
-(** QKvK position with white queen and black king
-    on distinct ranks *)
+
+(** QKvK position with WQ cutting off BK into a quadrant while not being
+    stalemated. This means that both the rank and file for WQ and BK are
+    distinct and furthermore, the WK is not interfering the WQ.
+**)
 Record t (s : ChessState) : Type := {
-  material : QKvK.t s;
-  rank_distinct : rank (white_queen material) <> rank (black_king s)
+  mat : QKvK.t s;
+  rank_distinct : rank (white_queen mat) <> rank (black_king s);
+  file_distinct : file (white_queen mat) <> file (black_king s);
+  no_rank_interference :
+    rank (white_queen mat) = rank (white_king s) ->
+    file_sbetween
+      (file (white_king s))
+      (file (white_queen mat))
+      (file (black_king s));
+  no_file_interference :
+    file (white_queen mat) = file (white_king s) ->
+    rank_sbetween
+      (rank (white_king s))
+      (rank (white_queen mat))
+      (rank (black_king s));
   }.
 
-Arguments material {_}.
+Arguments mat {_}.
 Arguments rank_distinct {_}.
+Arguments file_distinct {_}.
+Arguments no_rank_interference {_}.
+Arguments no_file_interference {_}.
 
-Lemma pforced : @pforces ChessGame White QKvK.t t.
+Definition sum1 {X} (F G : X -> Type) : X -> Type :=
+  fun X => (F X + G X)%type.
+
+Lemma pforced : @pforces ChessGame White QKvK.t (sum1 t (@awin ChessGame White)).
 Proof.
   intros s mat.
-  destruct (eq_dec (rank (white_queen mat)) (rank (white_king s))).
+  destruct (atomic_chess_res s) eqn:s_res.
+  - destruct r.
+    + destruct p.
+      * apply atom_force; right.
+        exact s_res.
+      * admit. (* easy contra *)
+    + elim (not_stalemate mat); auto.
+  - destruct (chess_to_play s) eqn:s_play.
+    + destruct (eq_dec (rank (white_queen mat)) (rank (black_king s))).
+      * destruct (eq_dec (rank (white_queen mat)) (rank (white_king s))).
+        -- admit.
+        -- eelim (opp_to_play_not_in_check s); rewrite s_play.
+           ++ apply lookup_black_king.
+           ++ exists (white_queen mat), Queen; split; [apply mat|].
+              right; left.
+              split; [exact e|].
+              intros p Hp1 Hp2.
+              destruct (lookup_piece p (board s)) eqn:Hp3; auto.
+              destruct p0 as [[|] pc].
+              ** apply (white_pieces mat) in Hp3.
+                 destruct Hp3; subst.
+                 --- admit. (* contra Hp2 *)
+                 --- elim n.
+                     apply Hp1.
+              ** apply (black_pieces mat) in Hp3; subst.
+                 admit. (* contra Hp2 *)
+      * destruct (eq_dec (file (white_queen mat)) (file (black_king s))).
+        -- admit.
+        -- apply atom_force; left.
+           unshelve econstructor; auto.
+           ++ admit.
+           ++ admit.
+    + admit.
 Admitted.
 
+
+
 End cutoff.
+
+Check cutoff.pforced.
 
 (* Lemma rank_ascending_forces :
   pforces White QKvK_white rank_ascending_QKvK_white.
