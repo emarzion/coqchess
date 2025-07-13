c4acc590bca1a1d51a181c817d14289f5b4abbb9
emarzion <emarzion@gmail.com>
2025-07-12 21:49:13 -0500

    last bits of material stuff

--- theory/TB/Material.v
+++ theory/TB/Material.v
@@ -201,15 +201,213 @@ Definition KRvK_bound (c : Player) (p : Piece) : nat :=
 Definition KRvK (s : Game.GameState ChessGame) : Prop :=
   forall c p, count c p (board s) <= KRvK_bound c p.
 
+Lemma vaccess_Vec_count_pos {X} `{Discrete X} {n} (x : X)
+  (v : Vec X n) (i : Fin.Fin n) :
+  Vec.vaccess i v = x ->
+  Vec_count x v > 0.
+Proof.
+  induction n; intro pf.
+  - destruct i.
+  - destruct i as [[]|i]; simpl in *.
+    + destruct eq_dec; [lia|].
+      congruence.
+    + destruct eq_dec; [lia|].
+      apply IHn with (i := i); auto.
+Qed.
+
+Lemma Vec_count_vupdate_sub {X} `{Discrete X} {n} (x y : X)
+  (v : Vec X n) (i : Fin.Fin n) :
+  Vec.vaccess i v <> x ->
+  Vec.vaccess i v = y ->
+  Vec_count y (Vec.vupdate i x v) =
+  pred (Vec_count y v).
+Proof.
+  induction n.
+  - destruct i.
+  - destruct v as [z v].
+    destruct i as [[]|i]; simpl; intros; subst.
+    + destruct eq_dec; [contradiction|].
+      destruct eq_dec; [|contradiction].
+      reflexivity.
+    + rewrite IHn; auto.
+      destruct eq_dec; auto.
+      pose proof (vaccess_Vec_count_pos (Vec.vaccess i v) v i eq_refl).
+      lia.
+Qed.
+
+Lemma Vec_count_vupdate_add {X} `{Discrete X} {n} (x : X)
+  (v : Vec X n) (i : Fin.Fin n) :
+  Vec.vaccess i v <> x ->
+  Vec_count x (Vec.vupdate i x v) =
+  S (Vec_count x v).
+Proof.
+  induction n.
+  - destruct i.
+  - destruct v as [z v].
+    destruct i as [[]|i]; simpl; intros; subst.
+    + destruct eq_dec; [|contradiction].
+      destruct eq_dec; [congruence|].
+      auto.
+    + rewrite IHn; auto.
+      destruct eq_dec; auto.
+Qed.
+
+Lemma Vec_count_vupdate_no_change {X} `{Discrete X} {n} (x y : X)
+  (v : Vec X n) (i : Fin.Fin n) :
+  Vec.vaccess i v <> x ->
+  Vec.vaccess i v <> y ->
+  x <> y ->
+  Vec_count y (Vec.vupdate i x v) =
+  Vec_count y v.
+Proof.
+  induction n.
+  - destruct i.
+  - destruct v as [z v].
+    destruct i as [[]|i]; simpl; intros.
+    + destruct eq_dec; [congruence|].
+      destruct (eq_dec y z); [congruence|].
+      auto.
+    + rewrite IHn; auto.
+Qed.
+
+Lemma Vec_sum_pos {n} (v : Vec nat n) (i : Fin.Fin n) :
+  vaccess i v > 0 ->
+  Vec_sum v > 0.
+Proof.
+  induction n; intro pf.
+  - destruct i.
+  - destruct i as [[]|i]; simpl in *.
+    + lia.
+    + apply IHn in pf; lia.
+Qed.
+
+Lemma maccess_Mat_count_pos {X} `{Discrete X} {m n} (x : X)
+  (M : Mat X m n) (c : Coord m n) :
+  Mat.maccess c M = x ->
+  Mat_count x M > 0.
+Proof.
+  intro pf; destruct c as [i j].
+  apply vaccess_Vec_count_pos in pf.
+  apply Vec_sum_pos with (i := i).
+  rewrite MatAction.vaccess_vmap; auto.
+Qed.
+
+Lemma Mat_count_mupdate_sub {X} `{Discrete X} {m n} (x y : X)
+  (M : Mat X m n) (c : Coord m n) :
+  Mat.maccess c M <> x ->
+  Mat.maccess c M = y ->
+  Mat_count y (Mat.mupdate c x M) =
+  pred (Mat_count y M).
+Proof.
+  destruct c as [i j].
+  unfold maccess, Mat_count, mupdate; simpl.
+  induction m.
+  - destruct i.
+  - destruct M as [c M'].
+    destruct i as [[]|i].
+    + simpl; intros pf1 pf2.
+      rewrite Vec_count_vupdate_sub; auto; simpl.
+      apply vaccess_Vec_count_pos in pf2; lia.
+    + simpl; intros pf1 pf2.
+      rewrite IHm; auto.
+      apply maccess_Mat_count_pos with (c := (i,j)) in pf2.
+      unfold Mat_count in pf2; lia.
+Qed.
+
+Lemma Mat_count_mupdate_add {X} `{Discrete X} {m n} (x : X)
+  (M : Mat X m n) (c : Coord m n) :
+  Mat.maccess c M <> x ->
+  Mat_count x (Mat.mupdate c x M) =
+  S (Mat_count x M).
+Proof.
+  destruct c as [i j].
+  unfold maccess, Mat_count, mupdate; simpl.
+  induction m.
+  - destruct i.
+  - destruct M as [c M'].
+    destruct i as [[]|i].
+    + simpl; intros.
+      rewrite Vec_count_vupdate_add; auto.
+    + simpl; intros.
+      rewrite IHm; auto.
+Qed.
+
+Lemma Mat_count_mupdate_no_change {X} `{Discrete X} {m n} (x y : X)
+  (M : Mat X m n) (c : Coord m n) :
+  Mat.maccess c M <> x ->
+  Mat.maccess c M <> y ->
+  x <> y ->
+  Mat_count y (Mat.mupdate c x M) =
+  Mat_count y M.
+Proof.
+  destruct c as [i j].
+  unfold maccess, Mat_count, mupdate; simpl.
+  induction m.
+  - destruct i.
+  - destruct M as [c M'].
+    destruct i as [[]|i].
+    + simpl; intros.
+      rewrite Vec_count_vupdate_no_change; auto.
+    + simpl; intros.
+      rewrite IHm; auto.
+Qed.
+
 Lemma count_exec_move {s} (m : ChessMove s) : forall c p,
   count c p (board (exec_ChessMove m)) <= count c p (board s).
 Proof.
-  unfold exec_ChessMove.
-  destruct m.
-  unfold exec_RegularMove.
+  intros; destruct m.
+  unfold exec_ChessMove, exec_RegularMove.
   simpl board.
-  unfold updated_board.
-Admitted.
+  unfold updated_board, count, clear.
+  unfold place_piece.
+  destruct (eq_dec (c, p) (chess_to_play s, piece (premove r))) as [Heq|Hneq].
+  - rewrite Mat_count_mupdate_sub.
+    + rewrite <- Heq.
+      rewrite Mat_count_mupdate_add.
+      * simpl; lia.
+      * pose proof (dest_open (premove_legal r)) as pf.
+        intro pf'.
+        unfold open in pf.
+        unfold lookup_piece in pf.
+        rewrite pf' in pf.
+        apply (opp_no_fp (chess_to_play s)); congruence.
+    + rewrite Mat.maccess_mupdate_neq.
+      * rewrite origin_lookup; [|apply r].
+        discriminate.
+      * intro; apply (dest_orig_neq r); congruence.
+    + rewrite Heq.
+      rewrite Mat.maccess_mupdate_neq.
+      * apply r.
+      * intro; apply (dest_orig_neq r); congruence.
+  - rewrite Mat_count_mupdate_no_change.
+    + destruct (eq_dec (Some (c, p)) (Mat.maccess (dest (premove r)) (board s)))
+        as [Heq|Hneq'].
+      * rewrite Mat_count_mupdate_sub; auto.
+        -- lia.
+        -- pose proof (dest_open (premove_legal r)) as pf.
+           unfold open in pf.
+           intro pf'.
+           unfold lookup_piece in pf.
+           rewrite pf' in pf.
+           apply (opp_no_fp (chess_to_play s)); congruence.
+      * rewrite Mat_count_mupdate_no_change; auto.
+        -- pose proof (dest_open (premove_legal r)) as pf.
+           unfold open in pf.
+           intro pf'.
+           unfold lookup_piece in pf.
+           rewrite pf' in pf.
+           apply (opp_no_fp (chess_to_play s)); congruence.
+        -- congruence.
+    + rewrite Mat.maccess_mupdate_neq.
+      * rewrite origin_lookup; [|apply r].
+        discriminate.
+      * intro; apply (dest_orig_neq r); congruence.
+    + rewrite Mat.maccess_mupdate_neq.
+      * rewrite origin_lookup; [|apply r].
+        congruence.
+      * intro; apply (dest_orig_neq r); congruence.
+    + discriminate.
+Qed.
 
 Lemma count_act x b : forall c p,
   count c p (x @ b) = count c p b.
